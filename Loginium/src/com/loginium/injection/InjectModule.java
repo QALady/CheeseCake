6c4537ce9cd58ab743bcf0153e691d737f7d5b9c
Thanh Le <thanh.viet.le@logigear.com>
2016-05-18 14:49:41 +0700

    a

---
+++ Loginium/src/com/loginium/injection/InjectModule.java
@@ -0,0 +1,20 @@
+package com.loginium.injection;
+
+import com.google.inject.AbstractModule;
+
+public class InjectModule  extends AbstractModule{
+
+  
+  
+  
+  @Override
+  protected void configure() {
+    // TODO Auto-generated method stub
+    
+  }
+  
+  
+  
+  
+  
+}
